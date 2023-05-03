789d32cd1cf84ce7195382b76a28eb44e51a7463
maishiro <16446436+maishiro@users.noreply.github.com>
2023-05-04 08:35:22 +0900

    webapi
    DBアクセス追加

--- webapi/controller/api_user.go
+++ webapi/controller/api_user.go
@@ -2,18 +2,76 @@ package controller
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"webapi/entity"
+	"webapi/model"
+
+	"github.com/gorilla/mux"
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	decoder := json.NewDecoder(r.Body)
+	var t model.User
+	err := decoder.Decode(&t)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-	objJSON := make(map[string]interface{})
-	objJSON["time"] = "2021-07-17 16:37:05"
-	objJSON["item1"] = "value1"
-	objJSON["item2"] = 1
-	objJSON["item3"] = 1.234
-	b, _ := json.Marshal(objJSON)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	var id int64 = 0
+	sql := `INSERT INTO "user" ("username", "firstname", "lastname", "email", "password", "phone", "userstatus") VALUES (?,?,?,?,?,?,?) RETURNING "id"`
+	result, err := model.Engine.SQL(sql, t.Username, t.FirstName, t.LastName, t.Email, t.Password, t.Phone, t.UserStatus).Get(&id)
+	if err != nil {
+		log.Printf("err: [%v]\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
+	log.Printf("result: [%v]\n", result)
+	log.Printf("id: [%v]\n", id)
+	t.Id = id
+
+	b, _ := json.Marshal(t)
 	w.Write(b)
+}
+
+func GetUserByName(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	user_name := vars["username"]
+	log.Printf("username: [%s]\n", user_name)
+
+	item := entity.User{}
+	result, err := model.Engine.Where("username = ?", user_name).Get(&item)
+	if err != nil {
+		log.Printf("err: [%v]\n", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	user := model.User{}
+	if !result {
+		log.Println("Not Found")
+	} else {
+		user = model.User{
+			Id:         item.Id,
+			Username:   item.Username,
+			FirstName:  item.FirstName,
+			LastName:   item.LastName,
+			Email:      item.Email,
+			Password:   item.Password,
+			Phone:      item.Phone,
+			UserStatus: item.UserStatus,
+		}
+	}
+
+	b, err := json.Marshal(user)
+	if err != nil {
+		log.Printf("err: [%v]\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write(b)
+	}
 }
---
+++ webapi/model/database.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"xorm.io/xorm"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	Engine *xorm.Engine
+)
+
+func NewConnectDB() error {
+	var err error
+	Engine, err = xorm.NewEngine("postgres", "postgres://postgres:postgres@localhost/postgres?sslmode=disable")
+	return err
+}
+
+func CloseDB() {
+	Engine.Close()
+}
