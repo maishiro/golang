356f0ab5b42e81e70f643450048e6611ab5a9f59
maishiro <16446436+maishiro@users.noreply.github.com>
2023-10-29 13:42:36 +0900

    update wails v2.6.0

--- wailsSample/main.go
+++ wailsSample/main.go
@@ -6,9 +6,10 @@ import (
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
-//go:embed frontend/dist
+//go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
@@ -21,7 +22,9 @@ func main() {
 		Title:            "wailsSample",
 		Width:            1024,
 		Height:           768,
-		Assets:           assets,
+		AssetServer: &assetserver.Options{
+			Assets:       assets,
+		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup: func(ctx context.Context) {
 			app.SetContext(ctx)
@@ -33,6 +36,7 @@ func main() {
 			socket,
 		},
 	})
+
 	if err != nil {
 		println("Error:", err.Error())
 	}
