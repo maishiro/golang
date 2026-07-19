4c9cfd73b38bb7a19f18f540f52b6541e187c523
maishiro <16446436+maishiro@users.noreply.github.com>
2026-07-19 22:02:14 +0900

    webapi
    add log file

--- webapi/main.go
+++ webapi/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"os"
 
 	"webapi/entity"
 
 	sw "webapi/server"
 
 	_ "github.com/lib/pq"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func main() {
+	fileLogger := &lumberjack.Logger{
+		Filename:   "./logs/webapi.log",
+		MaxSize:    10,
+		MaxBackups: 9,
+		LocalTime:  true,
+		Compress:   false,
+	}
+	multiWriter := io.MultiWriter(os.Stdout, fileLogger)
+	log.SetOutput(multiWriter)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	err := entity.NewConnectDB()
 	if err != nil {
 		log.Fatal(err)
